9a6c867f9c177daf42d1a243f5e0614390e03dc5
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-15 15:10:50 -0800

    put request to update previous workout with new exercise

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,7 +22,23 @@ module.exports = router => {
         });
     });
 
-   
+    // add exercise to a previous workout
+    router.put("/api/workouts/:id", (req, res) => {
+        Workout.findOneAndUpdate(
+            {
+                _id: req.params.id
+            },
+            { $push: { exercises: req.body } },
+            { new: true })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
+
+
+    
 
 
 };
