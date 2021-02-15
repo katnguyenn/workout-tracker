d876fd2015c3c26830f34e3b49efb991605aa048
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-15 14:56:53 -0800

    post request to create new workout

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,31 @@
 const router = require("express").Router();
-const db = require("../models/workoutModel");
+const Workout = require("../models/workoutModel");
 
-router.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
+// get workout
+module.exports = router => {
+    router.get("/api/workouts", (req, res) => {
+        Workout.find({}).then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
     });
 
-});
+    // create new workout
+    router.post("/api/workouts", (req, res) => {
+        Workout.create({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err)
+        });
+    });
+
+   
+
+
+};
 
 
 
@@ -17,13 +34,3 @@ router.get("/api/workouts", (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-module.exports = router;
