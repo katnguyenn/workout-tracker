34209736eb79a7f604dedb516eef3bc46d6cb621
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-15 16:35:33 -0800

    add virtual field for total duration

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
@@ -9,45 +8,64 @@ const WorkoutSchema = new Schema({
     },
     exercises: [
         {
-    type: {
-        type: String,
-        trim: true,
-        required: true
-    },
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
 
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
 
-    duration: {
-        type: Number,
-        required: true
-    },
+            duration: {
+                type: Number,
+                required: true
+            },
 
-    distance: {
-        type: Number,
-        required: true
-    },
+            distance: {
+                type: Number,
+                required: true
+            },
 
-    weight: {
-        type: Number,
-        required: true
-    },
+            weight: {
+                type: Number,
+                required: true
+            },
 
-    reps: {
-        type: Number,
-        required: true
-    },
+            reps: {
+                type: Number,
+                required: true
+            },
 
-    sets: {
-        type: Number,
-        required: true
+            sets: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
-}
-    ]
+   
+);
+
+
+
+// Total duration
+WorkoutSchema.virtual("totalDuration").get(function () {
+    const duration = this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+
+    return duration;
 });
+
 
 
 
